919246c8bd6d6131db95f54976ce2b11aa7cf9ca
mswalsh68 <mswalsh68@gmail.com>
2024-02-07 12:08:22 -0500

    Update ThankYou.jsx

--- src/Pages/ThankYou.jsx
+++ src/Pages/ThankYou.jsx
@@ -1,15 +1,19 @@
 import React from 'react'
 import '../styles.css'
+import { Link } from 'react-router-dom'
+
 
 export default function ThankYou() {
   return (
     <div className='main-body'>
         <h1>
-        Thank you for contacting us. Someone will be in touch with you shortly
+        Thank you for contacting us. I will be in touch with you shortly
         </h1>
     
     
-    
+        <Link to='/' >
+            <p>Back to Home</p>
+        </Link>
     
     
     </div>
