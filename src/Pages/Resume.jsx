a0b9532573f67a90bc6be33f7aba14c79e9a9bc7
mswalsh68 <mswalsh68@gmail.com>
2024-02-12 14:22:41 -0500

    C#

--- src/Pages/Resume.jsx
+++ src/Pages/Resume.jsx
@@ -1,5 +1,6 @@
 import '../styles.css'
 import resume2024 from '../assets/Mike Walsh 2024.pdf'
+import Csharp from '../assets/Mike Walsh Foundational CSharp Microsoft Certification.png'
 
 export default function Resume() {
 
@@ -10,6 +11,7 @@ export default function Resume() {
       <h1>Resume</h1>
       <div className='wrapper'>
       <iframe className='resume' src={resume2024}></iframe>
+        <img className='homeImages' src={Csharp}/>
       </div>
     </div>
   );
